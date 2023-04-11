3a57c1f285f0a0d1d78b267dc3d1f906f4245976
ajoy267 <andrew.joy12@hotmail.com>
2023-04-11 13:35:57 -0700

    created user context

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 import Login from './views/Auth/Login';
 import Home from './views/Home/Home';
 import Welcome from './views/Welcome/Welcome';
@@ -6,19 +7,21 @@ import Welcome from './views/Welcome/Welcome';
 export default function App() {
   return (
     <main>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Welcome />
-          </Route>
-          <Route path="/signup">
-            <Login />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <UserProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Welcome />
+            </Route>
+            <Route path="/signup">
+              <Login />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </UserProvider>
     </main>
   );
 }
