c251bb1f09f1f3c2523aa5e9f601a50e425c820b
ajoy267 <andrew.joy12@hotmail.com>
2023-04-12 10:36:08 -0700

    cleaning code

--- src/views/Welcome/Welcome.jsx
+++ src/views/Welcome/Welcome.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
 import WelcomeMessage from '../../components/Welcome/WelcomeMessage';
 
 export default function Welcome() {
