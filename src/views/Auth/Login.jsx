5e60157bbc258369002a407a501fb27a4d061113
ajoy267 <andrew.joy12@hotmail.com>
2023-04-12 11:03:16 -0700

    setting up back end with supabase

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoginForm from '../../components/Auth/LoginForm';
 
 export default function Login() {
   return (
     <>
-      <p>In progress</p>
+      <LoginForm />
     </>
   );
 }
