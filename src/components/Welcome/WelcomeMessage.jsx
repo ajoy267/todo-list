42c0cc3e1ac21c62d20a14fb90fd5aa8f65996f9
ajoy267 <andrew.joy12@hotmail.com>
2023-04-12 10:20:48 -0700

    added a header and little message on welcome page

---
+++ src/components/Welcome/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function WelcomeMessage() {
+  return (
+    <main>
+      <h3>Welcome to my little task management app!</h3>
+    </main>
+  );
+}
--- src/views/Welcome/Welcome.jsx
+++ src/views/Welcome/Welcome.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import WelcomeMessage from '../../components/Welcome/WelcomeMessage';
 
 export default function Welcome() {
-  return <div>Welcome</div>;
+  return (
+    <>
+      <Header />
+      <WelcomeMessage />
+    </>
+  );
 }
