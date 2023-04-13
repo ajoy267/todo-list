4b0a000b89ee0a209b35a6cb73bcdc42d35d3e15
ajoy267 <andrew.joy12@hotmail.com>
2023-04-13 13:50:30 -0700

    got my sign out button to work in header and also got it to change to sign in if user is logged out

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
+import { signOutUser } from '../../services/users';
 
 export default function Header() {
+  const { user, setUser } = useUser();
+  const handleLogout = () => {
+    signOutUser();
+    setUser();
+  };
+
   return (
     <header>
-      <h1>To-Do List</h1>
-      <button>Sign In</button>
+      <Link to="/home">
+        <h1>To-Do List</h1>
+      </Link>
+      {user ? (
+        <Link to="/">
+          <button onClick={handleLogout}>Sign Out</button>
+        </Link>
+      ) : (
+        <Link to="/signin">
+          <button>Sign In</button>
+        </Link>
+      )}
     </header>
   );
 }
